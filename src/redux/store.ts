1aba1d2cd80c6bbc2a6ec2fc756e96befb41eb7f
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-05 00:00:10 +0600

    befor commit integrate redux persist and now solve serealize and non serialize object

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./features/auth/authSlice"
 import { baseApi } from "./api/baseApi"
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore,
+     persistReducer,
+     FLUSH,
+     REHYDRATE,
+     PAUSE,
+     PERSIST,
+     PURGE,
+     REGISTER, } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -19,7 +26,11 @@ export const store=configureStore({
         [baseApi.reducerPath]:baseApi.reducer,
         auth:persistedAuthReducer
     },
-    middleware:getDefaultMiddleWares=>getDefaultMiddleWares().concat(baseApi.middleware)
+    middleware:getDefaultMiddleWares=>getDefaultMiddleWares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware)
 })
 
 export const  persistor=persistStore(store)
