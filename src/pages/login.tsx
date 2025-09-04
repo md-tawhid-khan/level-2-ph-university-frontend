d4fc8278f874999dd8141f383016838cd2c01dd2
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-04 19:47:42 +0600

    basic login form and useLoginMutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,53 @@
+import { useForm, type SubmitHandler } from "react-hook-form"
+import { useLoginMutation } from "../redux/features/auth/authApi"
+
+type Inputs = {
+  id: string
+  password: string
+}
+
 const Login=()=>{
+
+    const {
+    register,
+    handleSubmit,
+     formState: { errors },
+  } = useForm<Inputs>()
+
+  const [login,{data,error,isLoading}]=useLoginMutation()
+
+  if(isLoading){
+    return <h1>loading -------------------</h1>
+  }
+
+  console.log(data)
+  console.log(error)
+
+   const onSubmit: SubmitHandler<Inputs> = (data) => {
+
+    const {id,password}=data ;
+        login({
+            id,password
+        })
+}
+
     return (
-        <div>This is login page</div>
+       
+    <form onSubmit={handleSubmit(onSubmit)} >
+       
+       <label>user Id</label>
+
+      <input  {...register("id")} />
+
+      <label>password</label>
+
+      <input {...register("password", { required: true })} />
+     
+      {errors.password && <span>This field is required</span>}
+
+      <input type="submit" />
+    
+    </form>
     )
 }
 
