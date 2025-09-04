257584e0d205b5d23075848ae74d3faca75e97fb
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-04 23:12:15 +0600

    intregratte

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/routers.js';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
     <RouterProvider router={router}/>
+    </PersistGate>
     </Provider>
   </StrictMode>,
 )
