ca1dbc7d0457df867bd1563308feef7cf9c926e5
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-07 00:52:14 +0600

    create new access token by using refresh token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery=fetchBaseQuery({ 
         baseUrl: 'http://localhost:4003/api/v1',
@@ -15,7 +16,26 @@ const baseQuery=fetchBaseQuery({
 
     const baseQueryWithRefreshToken=async(args , api , extraOption)=>{
         const result=await baseQuery(args,api,extraOption)
-        console.log(result)
+        if(result.error?.status === 401){
+            console.log("sending refresh token")
+            const res=await fetch('http://localhost:4003/api/v1/auth/refresh-token',{
+                method:'POST',
+                credentials:'include'
+            })
+              
+
+            const data=await res.json()
+
+            console.log({data})
+
+            const user=(api.getState() as RootState ).auth.user
+            
+            api.dispatch(setUser({
+                user,
+                token:data.data.accessToken
+            }))
+
+        }
        return result
     }
 
